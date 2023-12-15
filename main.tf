e1b16549eeb075cbbc7cb7ebf7b9bd1a146e8319
satya <satya.bhimuni123@gmail.com>
2023-12-15 11:14:31 +0530

    project

--- main.tf
+++ main.tf
@@ -1,2 +1,15 @@
+resource "aws_ssm_parameter" "f11" {
+  count = length(var.params)
+  name = var.params[count.index].name
+  type = var.params[count.index].type
+  value = var.params[count.index].value
+}
+
+variable "params" {
+  default = [
+    { name: "Seetha", type: "String", value: "Seetha@123" },
+    { name: "Gopal", type: "String", value: "Gopal@123" },
+  ]
+}
 
 
